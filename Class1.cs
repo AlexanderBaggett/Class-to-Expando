de0a1c92e037267a0a828a96172a660d5af44043
Alexander Baggett <alexander.baggett@gmail.com>
2017-03-02 16:17:38 -0600

    Added files

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Dynamic;
+using System.Collections.Generic;
+public class Class1
+{
+    public static dynamic ToExpando(Type t, Object o)
+    {
+        dynamic Properties = new ExpandoObject();
+        var expandoDictionary = Properties as IDictionary<string, object>;
+        PropertyInfo[] propertyArray = t.GetProperties();
+        foreach (PropertyInfo property in propertyArray)
+        {
+            if (property.GetValue(o, null) != null)
+            {
+                expandoDictionary.Add(property.Name, property.GetValue(o, null));
+            }
+        }
+        return Properties;
+    }
+
+}
